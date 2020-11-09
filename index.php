0cee95f2c1571ad81a0ff046a05d222db3bda065
Renier Trenuela <rtrenuela@objectbright.com>
2020-11-09 14:53:59 +0800

    fix labe home

--- index.php
+++ index.php
@@ -55,7 +55,7 @@
 
 				<!--  Main navigation  -->
 				<ul class="main-nav nav navbar-nav navbar-right">
-					<li><a href="#home">Homes</a></li>
+					<li><a href="#home">Home</a></li>
 					<li><a href="#about">About</a></li>
 					<li><a href="#service">Services</a></li>
 					<li><a href="#portfolio">Recruitment Office</a></li>

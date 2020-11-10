35a30926653e19059609a63ef59f90fe165318f4
Mary Joy Castillo <35132948+Castillo14@users.noreply.github.com>
2020-11-10 05:58:40 -0500

    logo and images

--- index.php
+++ index.php
@@ -23,6 +23,77 @@
 
 	<!-- Custom stlylesheet -->
 	<link type="text/css" rel="stylesheet" href="style.css" />
+<style>
+	* {box-sizing: border-box;}
+body {font-family: Verdana, sans-serif;}
+.mySlides {display: none;}
+img {vertical-align: middle;}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+
+/* Caption text */
+.text {
+  color: #f2f2f2;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 12px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 3.5s;
+  animation-name: fade;
+  animation-duration: 3.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .text {font-size: 11px}
+}
+</style>
 </head>
 <body>
 	<header id="home">
@@ -41,7 +112,7 @@
 					<div class="navbar-brand">
 						<a href="index.php">
 							<img class="logo" src="logosim.png" alt="logo">
-							<img class="logo-alt" src="logosim_2.png" alt="logo">
+							<img class="logo-alt" src="logosim.png" alt="logo">
 						</a>
 					</div>
 					<!-- /Logo -->
@@ -202,16 +273,50 @@
 					</div>
 				</div>
 				<!-- /why choose us content -->
-
-				<!-- About slider -->
 				<div class="col-md-6">
+				<div class="mySlides fade">
+				  <div class="numbertext">1 / 4</div>
+				  <img src="makati.png" style="width:100%">
+				  <div class="text">Caption Text</div>
+				</div>
+
+				<div class="mySlides fade">
+				  <div class="numbertext">2 / 4</div>
+				  <img src="cagayandeoro.png" style="width:100%">
+				  <div class="text">Caption Two</div>
+				</div>
+
+				<div class="mySlides fade">
+				  <div class="numbertext">3 / 4</div>
+				  <img src="butuan.png" style="width:100%">
+				  <div class="text">Caption Three</div>
+				</div>
+
+				<div class="mySlides fade">
+				  <div class="numbertext">4 / 4</div>
+				  <img src="digos.png" style="width:100%">
+				  <div class="text">Caption Three</div>
+				</div>
+
+				</div>
+				<br>
+
+				<div style="text-align:center">
+				  <span class="dot"></span> 
+				  <span class="dot"></span> 
+				  <span class="dot"></span> 
+				   <span class="dot"></span> 
+				</div>
+			</div>
+				<!-- About slider -->
+				<!-- <div class="col-md-6">
 					<div id="about-slider" class="owl-carousel owl-theme">
 						<img class="img-responsive" src="makati.png" alt="">
 						<img class="img-responsive" src="cagayandeoro.png" alt="">
 						<img class="img-responsive" src="butuan.png" alt="">
 						<img class="img-responsive" src="digos.png" alt="">
 					</div>
-				</div>
+				</div> -->
 				<!-- /About slider -->
 
 			</div>
@@ -705,7 +810,27 @@
 			<span></span>
 		</div>
 	</div> -->
+	<script>
+var slideIndex = 0;
+showSlides();
 
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+</script>
 	<script type="text/javascript" src="js/jquery.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="js/owl.carousel.min.js"></script>
